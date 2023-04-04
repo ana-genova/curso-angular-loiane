717c90e34ff136334b24b0594e3b80e2cbb787c3
ana.genova <anagenova9@gmail.com>
2023-04-04 19:17:28 -0300

    Aula #6 Introdução templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http://loiane.training'
+  }
 
   ngOnInit(): void {
   }
